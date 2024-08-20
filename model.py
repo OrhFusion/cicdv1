cb2e9c64482ff4250293fee24a45bdada35197f8
OrhFusion <moinulhaq.ml@outlook.com>
2024-08-20 20:05:19 +0530

    try.v7

--- model.py
+++ model.py
@@ -1,7 +1,4 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
