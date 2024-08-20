8ff302c9d21df3587a24a21b5a559e5d3635f986
OrhFusion <moinulhaq.ml@outlook.com>
2024-08-20 21:00:05 +0530

    try.v16

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
-"""
-This module defines a Flask web application for predicting placement packages
-based on CGPA.
-"""
-
 from flask import Flask, render_template, request
 import joblib
 import pandas as pd
 
 # Load the pre-trained model
-model = joblib.load('model.joblib')  # Assuming 'model.joblib' is in the same directory
+model = joblib.load('model.joblib') 
 
 app = Flask(__name__)
 
@@ -22,8 +17,6 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     """
-    Handles the POST request for predicting the placement package based on CGPA.
-    Extracts the CGPA from the form data, makes a prediction, and renders the
     result on the homepage.
     """
     if request.method == 'POST':
@@ -44,11 +37,13 @@ def predict():
         except ValueError:
             # Handle cases where CGPA is not a valid float
             return render_template(
-                'index.html', prediction="Invalid input. Please enter a valid number."
+                'index.html', prediction="Invalid"
             )
 
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)  # Or any other available port
+    app.run(host='0.0.0.0', port=5000)  
+
+
 
